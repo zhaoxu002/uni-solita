360817516cb905b43f047ae3414c8180d7159106
zx <zx754753023@gmail.com>
2023-08-29 18:23:19 +1200

    feat: :sparkles: excel string length

--- cloudfunctions/order/utils/getItemDescription.js
+++ cloudfunctions/order/utils/getItemDescription.js
@@ -1,7 +1,7 @@
 module.exports = function getItemDescription(items) {
   return items.reduce((dest, item) => {
     const { itemName, itemQuantity } = item;
-    dest += `${itemName.substring(0, 32)}*${itemQuantity} `;
+    dest += `${itemName.substring(0, 64)} * ${itemQuantity} `;
     return dest;
   }, "");
 };
